23748ea09c3033ae7d2a3eeaeb17aca16fba2b70
Alejandra Gonzalez <aleja.gonzalezv19@gmail.com>
2021-06-28 09:35:33 -0500

    Timeout fix

--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -4,7 +4,7 @@ import { reporter } from './helpers/reporter';
 export const config: Config = {
   framework: 'jasmine',
   specs: ['../test/**/*.spec.js'],
-  getPageTimeout: 3000,
+  getPageTimeout: 1000,
   SELENIUM_PROMISE_MANAGER: false,
   onPrepare: () => {
     reporter();
