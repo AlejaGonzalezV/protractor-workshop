cfd4cf2a3a9073a4287bc96d366bb5914069fc09
Alejandra Gonzalez Velez <alejandra.gonzalezv@perficient.com>
2021-07-02 17:18:27 -0500

    Elements filter

--- src/page/product-list.page.ts
+++ src/page/product-list.page.ts
@@ -1,16 +1,31 @@
 import {
-  $, ElementFinder, ExpectedConditions, browser,
+  $, $$, ElementArrayFinder, ElementFinder, ExpectedConditions, browser,
 } from 'protractor';
 
 export class ProductListPage {
-  private addToCartMenu: ElementFinder;
+  private addToCartBtn: ElementFinder;
+
+  private products: ElementArrayFinder;
 
   constructor() {
-    this.addToCartMenu = $('#center_column [Title="Add to cart"]');
+    this.products = $$('.product_list.row');
+    this.addToCartBtn = $('#add_to_cart > button');
+  }
+
+  private findByProduct(productName: string): ElementFinder {
+    return this.products.filter(
+      (product: ElementFinder) => product.$('[itemprop="name"]').getText().then((text : string) => text === productName)
+    ).first();
+  }
+
+  public async selectProduct(productName: string): Promise<void> {
+    const product = this.findByProduct(productName);
+    await browser.wait(ExpectedConditions.elementToBeClickable(product.$('img')), 3000);
+    product.$('img').click();
   }
 
   public async goToAddToCart(): Promise<void> {
-    await browser.wait(ExpectedConditions.elementToBeClickable(this.addToCartMenu), 3000);
-    await this.addToCartMenu.click();
+    await browser.wait(ExpectedConditions.elementToBeClickable(this.addToCartBtn), 3000);
+    await this.addToCartBtn.click();
   }
 }
