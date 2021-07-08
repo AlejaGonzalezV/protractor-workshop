c8df314b601f1c2b8c8e4651fabc4a93728f0bbb
Alejandra Gonzalez Velez <alejandra.gonzalezv@perficient.com>
2021-07-08 13:34:07 -0500

    iFrames tests

--- test/i-frame.spec.ts
+++ test/i-frame.spec.ts
@@ -4,19 +4,44 @@ import {
 } from '../src/page';
 
 describe('Given a practice iframe page', () => {
+  const iFramePage: IFramePage = new IFramePage();
+
   beforeAll(async () => {
     browser.waitForAngularEnabled(false);
     await browser.get('https://demoqa.com/frames');
   });
 
+  it('Then it should have the title', async () => {
+    expect(await iFramePage.getTitle()).toEqual('Frames');
+  });
+
   describe('When the height of an iframe is modified', () => {
-    const iFramePage: IFramePage = new IFramePage();
     beforeAll(async () => {
       await iFramePage.setFormFrameHeight(500);
     });
 
     it('Then it should have a new height', async () => {
       expect(await iFramePage.getIframeHeight()).toEqual(500);
+    });
+  });
+
+  describe('When switch to frame 1', () => {
+    beforeAll(async () => {
+      await iFramePage.switchToFrame();
+    });
+
+    it('Then it should have the title', async () => {
+      expect(await iFramePage.getTitle()).toEqual('This is a sample page');
+    });
+
+    describe('When switch to main page', () => {
+      beforeAll(async () => {
+        await iFramePage.switchToMainPage();
+      });
+
+      it('Then it should have the title', async () => {
+        expect(await iFramePage.getTitle()).toEqual('Frames');
+      });
     });
   });
 });
